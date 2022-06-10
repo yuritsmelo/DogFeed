e87e98758cea2e9339cfc53b2e6511a8fc6e1caf
yuritsmelo <yuri.melo@ucsal.edu.br>
2022-06-10 11:56:54 -0300

    add botões

--- app/src/main/java/br/ucsal/mobile/dogfeed/overview/OverviewFragment.kt
+++ app/src/main/java/br/ucsal/mobile/dogfeed/overview/OverviewFragment.kt
@@ -1,13 +1,15 @@
 package br.ucsal.mobile.dogfeed.overview
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import br.ucsal.mobile.dogfeed.R
 import br.ucsal.mobile.dogfeed.databinding.FragmentOverviewBinding
-import br.ucsal.mobile.dogfeed.databinding.GridViewItemBinding
 
 class OverviewFragment : Fragment(){
 
@@ -26,9 +28,10 @@ class OverviewFragment : Fragment(){
 
         binding.photosGrid.adapter = PhotoGridAdapter()
 
+        binding.buttonNew.setOnClickListener{viewModel.getDogPhotos()}
+
+        binding.switchFav.setOnClickListener{Toast.makeText(this.context, "added to favs", Toast.LENGTH_SHORT).show()}
+
         return binding.root
     }
 }
-
-
-
--- app/src/main/java/br/ucsal/mobile/dogfeed/overview/OverviewViewModel.kt
+++ app/src/main/java/br/ucsal/mobile/dogfeed/overview/OverviewViewModel.kt
@@ -1,6 +1,5 @@
 package br.ucsal.mobile.dogfeed.overview
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,18 +12,18 @@ class OverviewViewModel : ViewModel(){
     private val _status = MutableLiveData<String>()
     private val _photos = MutableLiveData<List<DogPhoto>?>()
 
-    val status: LiveData<String> = _status
+    //val status: LiveData<String> = _status
     val photos: MutableLiveData<List<DogPhoto>?> = _photos
 
     init {
         getDogPhotos()
     }
 
-    suspend fun toList(): List<DogPhoto> {
+    private suspend fun toList(): List<DogPhoto> {
         return listOf(DogApi.retrofitService.getPhotos())
     }
 
-    private fun getDogPhotos() {
+    fun getDogPhotos() {
         viewModelScope.launch {
             try {
                 _photos.value = toList()
